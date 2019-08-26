3065026f8c3ad72a6021b98fc25e2a43da0659ec
bbc539ff <bbc539ff@protonmail.com>
2019-08-26 19:05:57 +0800

    Update user info.

--- chocoshop_model/src/main/java/com/chocoshop/model/UserInfo.java
+++ chocoshop_model/src/main/java/com/chocoshop/model/UserInfo.java
@@ -4,6 +4,7 @@ import com.chocoshop.model.SysRole;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -11,12 +12,21 @@ public class UserInfo implements Serializable {
     @Id
     @GeneratedValue
     private Integer uid;
-    @Column(unique =true)
+    @Column(unique = true)
     private String username;//帐号
     private String name;//名称（昵称或者真实姓名，不同系统不同定义）
     private String password; //密码;
     private String salt;//加密密码的盐
     private byte state;//用户状态,0:创建未认证（比如没有激活，没有输入验证码等等）--等待验证的用户 , 1:正常状态,2：用户被锁定.
+    private String phone;
+    private String email;
+    private Date created;
+    private Date updated;
+    private Boolean gender;
+    private String address;
+    private String photo;
+
+
     @ManyToMany(fetch= FetchType.EAGER)//立即从数据库中进行加载数据;
     @JoinTable(name = "SysUserRole", joinColumns = { @JoinColumn(name = "uid") }, inverseJoinColumns ={@JoinColumn(name = "roleId") })
     private List<SysRole> roleList;// 一个用户具有多个角色
@@ -77,6 +87,62 @@ public class UserInfo implements Serializable {
         this.roleList = roleList;
     }
 
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
     /**
      * 密码盐.
      * @return String
@@ -96,6 +162,13 @@ public class UserInfo implements Serializable {
                 ", password='" + password + '\'' +
                 ", salt='" + salt + '\'' +
                 ", state=" + state +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                ", created=" + created +
+                ", updated=" + updated +
+                ", gender=" + gender +
+                ", address='" + address + '\'' +
+                ", photo='" + photo + '\'' +
                 ", roleList=" + roleList +
                 '}';
     }
