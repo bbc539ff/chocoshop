69e898231ad83a6a02efafeef43809d47fbaa39a
bbc539ff <bbc539ff@protonmail.com>
2019-08-28 13:37:01 +0800

    update

--- chocoshop_web/src/main/java/com/chocoshop/config/ShiroConfig.java
+++ chocoshop_web/src/main/java/com/chocoshop/config/ShiroConfig.java
@@ -8,6 +8,7 @@ import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
 
 import java.util.LinkedHashMap;
@@ -24,7 +25,8 @@ public class ShiroConfig {
         //拦截器.
         Map<String,String> filterChainDefinitionMap = new LinkedHashMap<>();
         // 配置不会被拦截的链接 顺序判断
-        filterChainDefinitionMap.put("/static/**", "anon");
+        filterChainDefinitionMap.put("/img/**", "anon");
+        filterChainDefinitionMap.put("/register", "anon");
         //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了
         filterChainDefinitionMap.put("/logout", "logout");
         //<!-- 过滤链定义，从上向下顺序执行，一般将/**放在最为下边 -->:这是一个坑呢，一不小心代码就不好使了;
--- chocoshop_web/src/main/java/com/chocoshop/controller/UserController.java
+++ chocoshop_web/src/main/java/com/chocoshop/controller/UserController.java
@@ -2,6 +2,7 @@ package com.chocoshop.controller;
 
 import com.chocoshop.model.UserInfo;
 import com.chocoshop.service.UserInfoService;
+import com.chocoshop.utils.Utils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authz.annotation.RequiresAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 @Controller
 public class UserController {
@@ -19,11 +21,21 @@ public class UserController {
     @Autowired
     UserInfoService userInfoService;
 
+    /**
+     * 主页面
+     * @return
+     */
     @GetMapping({"/", "index"})
     public String index(){
         return "index";
     }
 
+    /**
+     * 登录页面（只处理失败情况）
+     * @param request
+     * @param user
+     * @return
+     */
     @RequestMapping("/login")
     public String login(HttpServletRequest request, UserInfo user) {
         System.out.println(user);
@@ -36,6 +48,43 @@ public class UserController {
         return "/login";
     }
 
+    /**
+     * 用户注册页面
+     * @param
+     * @return
+     */
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    /**
+     * 提交注册表单
+     * @param userInfo
+     * @return
+     */
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String register(UserInfo userInfo){
+        System.out.println(userInfo);
+        String pwd = userInfo.getPassword();
+        String salt = Utils.generateSalt(pwd);
+        String new_pwd = Utils.generatePwd(pwd, salt);
+        userInfo.setSalt(salt);
+        userInfo.setPassword(new_pwd);
+        userInfo.setState(false); // 默认为不可用
+        userInfo.setCreated(new Date());
+        userInfo.setUpdated(new Date());
+
+        userInfoService.createUser(userInfo);
+        return "redirect:/login";
+    }
+
+    /**
+     * 用户详细信息页面
+     * @param username
+     * @param model
+     * @return
+     */
     @RequestMapping("/userinfo/{username}")
     public String showUserInfo(@PathVariable String username, Model model){
         UserInfo userinfo = userInfoService.findByUsername(username);
@@ -43,10 +92,4 @@ public class UserController {
         model.addAttribute("userinfo", userinfo);
         return "userinfo";
     }
-
-    public String register(UserInfo userInfo){
-    return "";
-    }
-
-
 }
